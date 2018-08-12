d08ddee7e3f494d8ac0c5e3ce7b96494bcf6a597
Daniel Carvalho <danieljosecarvalho@gmail.com>
2018-08-12 11:52:17 -0300

    Melhorias no código, utilizando libs de botao e timer

--- prototipo_1.ino
+++ prototipo_1.ino
@@ -1,99 +1,196 @@
-#include <DS1307.h>
+#include <Wire.h>
+#include <DS1307RTC.h>
+#include <Time.h>
+#include <TimeLib.h>
+#include <TimeAlarms.h>
+#include <AceButton.h>
+#include <AdjustableButtonConfig.h>
+#include <ButtonConfig.h>
 #include "ACS712.h"
-
 #include <ESP8266WiFi.h>
 #include "ESP8266FtpServer.h"
 
-#define LED_POWER D3
+#define LED_POWER   D3
+#define BUTTON_PIN  D5
+
+using namespace ace_button;
 
 ACS712 sensor(ACS712_30A, A0);
-DS1307 rtc(D2, D1);
+//DS1307 rtc(D2, D1);
+tmElements_t tm;
 FtpServer ftp_srv;
+AceButton button(BUTTON_PIN);
 
-const char *ssid          = "JuaWMS";
-const char *password      = "juawms2018";
-const float V             = 220;
-const float treshold      = 1.0;
-bool ligado               = false;
+const char *ssid              = "JuaWMS";
+const char *password          = "";
+const char *ftp_user          = "juawms";
+const char *ftp_password      = "juawms";
+const double V                = 220;
+const double treshold         = 1.0;
+bool ligado                   = false;
+bool telemetria               = false;
+bool servidor_ftp             = false;
+int led_state                 = HIGH;
+unsigned int interval         = 1000;
+unsigned int previous_millis  = 0;
+AlarmId alarm_id_sensor;
 
-unsigned int tempo_atual  = millis();
+void modo_telemetria();
+void modo_sensor();
+void handle_event(AceButton*, uint8_t, uint8_t);
 
 void setup() {
     pinMode(LED_POWER, OUTPUT);
-    digitalWrite(LED_POWER, HIGH);
-     //Aciona o relogio
-    rtc.halt(false);
+    digitalWrite(LED_POWER, led_state);
     
-    //As linhas abaixo setam a data e hora do modulo
-    //e podem ser comentada apos a primeira utilizacao
-//    rtc.setDOW(FRIDAY);      //Define o dia da semana
-//    rtc.setTime(22, 49, 0);     //Define o horario
-//    rtc.setDate(27, 7, 2018);   //Define o dia, mes e ano
-    
-    //Definicoes do pino SQW/Out
-    rtc.setSQWRate(SQW_RATE_1);
-    rtc.enableSQW(true);
-    
-    Serial.begin(115200);
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
 
-    // Configurando o access point
-    WiFi.softAP(ssid, password);
-    IPAddress myIP = WiFi.softAPIP();
-    Serial.print("AP IP address: ");
-    Serial.println(myIP);
-    
+    ButtonConfig* button_config = button.getButtonConfig();
+    button_config->setEventHandler(handle_event);
+    button_config->setFeature(ButtonConfig::kFeatureClick);
+    button_config->setFeature(ButtonConfig::kFeatureLongPress);
+
+    bool parse = false;
+    bool config = false;
+
+// get the date and time the compiler was run
+//    if (getDate(__DATE__) && getTime(__TIME__)) 
+//    {
+//      parse = true;
+//      // and configure the RTC with this info
+//      if (RTC.write(tm))
+//      {
+//        config = true;
+//      }
+//    }
+  
+    Serial.begin(115200);
+    //while (!Serial) ; // wait for Arduino Serial Monitor
+    //delay(200);
+    if (parse && config) 
+    {
+      Serial.print("DS1307 configured Time=");
+      Serial.print(__TIME__);
+      Serial.print(", Date=");
+      Serial.println(__DATE__);
+    }
+
     if (SPIFFS.begin())
     {
       Serial.println("SPIFFS opened!");
-//      SPIFFS.format();
-      ftp_srv.begin("juawms", "juawms");
+//    SPIFFS.format();
+      ftp_srv.begin(ftp_user, ftp_password);
       File f = SPIFFS.open("/coleta.csv", "a");
       f.println("<<< SISTEMA INICIADO >>>");
       f.close();
     }
+    
     Serial.println("Aguarde. Calibrando...");
     sensor.calibrate();
     Serial.println("Fim da calibração");
+
+    alarm_id_sensor = Alarm.timerRepeat(1, modo_sensor);
 }
 
-void loop() {
-    ftp_srv.handleFTP();
-    
-    if (millis() - tempo_atual >= 1000)
+void loop() 
+{
+  button.check();
+  if (telemetria)
+  {
+    unsigned long current_millis = millis();
+    if(current_millis - previous_millis >= interval)
     {
-      float I = sensor.getCurrentAC(60);
-      if (I > treshold && !ligado)
-      {
-        File f = SPIFFS.open("/coleta.csv", "a");
-        f.print(rtc.getTimeStr());
-        f.print(",");
-        f.print(rtc.getDateStr(FORMAT_SHORT));
-        f.print(",");
-        f.println("1");
-        f.close();
-        tempo_atual = millis();
-        ligado = true;
-        Serial.println(String("Corrente = ") + I + " A");
-      }
-      else if (I < treshold && ligado)
-      {
-        File f = SPIFFS.open("/coleta.csv", "a");
-        f.print(rtc.getTimeStr());
-        f.print(",");
-        f.print(rtc.getDateStr(FORMAT_SHORT));
-        f.print(",");
-        f.println("0");
-        f.close();
-        tempo_atual = millis();
-        ligado = false;
-        Serial.println(String("Corrente = ") + I + " A");
-      }
+      previous_millis = current_millis;   
+      if (led_state == LOW)
+        led_state = HIGH;  // Note that this switches the LED *off*
       else
-      {
-        tempo_atual = millis();
-      }
-      Serial.print("Corrente (A): ");
-      Serial.println(I);
+        led_state = LOW;   // Note that this switches the LED *on*
+      digitalWrite(LED_POWER, led_state);
     }
+  }
+  else
+  {
+    if (!led_state)
+    {
+      led_state = HIGH;
+      digitalWrite(LED_POWER, led_state);
+    }
+
+    if (servidor_ftp)
+      servidor_ftp = false;
+  }
+}
+
+void modo_telemetria()
+{
+  // Desabilitando as chamadas da função de escrita no arquivo
+  Alarm.disable(alarm_id_sensor);
+  // Inicializando o servidor FTP
+  if (!servidor_ftp)
+  {
+    ftp_srv.begin(ftp_user, ftp_password);
+    servidor_ftp = true; 
+  }
+  ftp_srv.handleFTP();
+  if (WiFi.status() != WL_CONNECTED)
+    WiFi.begin(ssid, password); 
+}
+
+void modo_sensor()
+{
+  float I = sensor.getCurrentAC(60);
+  if (I > treshold && !ligado)
+  {
+    // Formato: TIMESTAMP,<ON/OFF>,CORRENTE(A)
+    File f = SPIFFS.open("/coleta.csv", "a");
+//    f.print(rtc.getTimeStr());
+//    f.print(",");
+//    f.print(rtc.getDateStr(FORMAT_SHORT));
+    f.print(",");
+    f.print("1");
+    f.print(",");
+    f.println(I);
+    f.close();
+    ligado = true;
+    Serial.println(String("Corrente = ") + I + " A");
+  }
+  else if (I < treshold && ligado)
+  {
+    File f = SPIFFS.open("/coleta.csv", "a");
+//    f.print(rtc.getTimeStr());
+//    f.print(",");
+//    f.print(rtc.getDateStr(FORMAT_SHORT));
+    f.print(",");
+    f.print("0");
+    f.print(",");
+    f.println(I);
+    f.close();
+    ligado = false;
+    Serial.println(String("Corrente = ") + I + " A");
+  }
+  Serial.print("Corrente (A): ");
+  Serial.println(I);
+}
+
+void handle_event(AceButton* /* button */, uint8_t eventType, uint8_t buttonState)
+{
+
+  // Print out a message for all events.
+  Serial.print(F("handleEvent(): eventType: "));
+  Serial.print(eventType);
+  Serial.print(F("; buttonState: "));
+  Serial.println(buttonState);
+
+  switch (eventType)
+  {
+    case AceButton::kEventClicked:
+      telemetria = false;
+      modo_sensor();
+      break;
+    case AceButton::kEventLongPressed:
+      telemetria = true;
+      modo_telemetria();
+      break;
+  }
 }
 
