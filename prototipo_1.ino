1e4938a21b1539633d422111f7e72c08a2a89853
Daniel Carvalho <danieljosecarvalho@gmail.com>
2018-10-04 20:25:27 -0300

    Atualização

--- prototipo_1.ino
+++ prototipo_1.ino
@@ -24,9 +24,8 @@ const char *password          = "";
 const char *ftp_user          = "juawms";
 const char *ftp_password      = "juawms";
 const double V                = 220;
-const double treshold         = 1.0;
+const double treshold         = 1.00;
 bool ligado                   = false;
-//bool telemetria               = false;
 bool servidor_ftp             = false;
 int led_state                 = HIGH;
 unsigned int interval         = 1000;
@@ -40,7 +39,8 @@ void blink_led();
 void led_on();
 void handle_event(AceButton*, uint8_t, uint8_t);
 
-void setup() {
+void setup() 
+{
     pinMode(LED_POWER, OUTPUT);
     pinMode(BUTTON_PIN, INPUT_PULLUP);
     digitalWrite(LED_POWER, led_state);
@@ -49,27 +49,30 @@ void setup() {
     button_config->setEventHandler(handle_event);
     button_config->setFeature(ButtonConfig::kFeatureClick);
     button_config->setFeature(ButtonConfig::kFeatureLongPress);
-
-   WiFi.mode(WIFI_AP);//Define o ESP8266 como Acess Point.
-   WiFi.softAP(ssid, password);//Cria um WiFi de nome "NodeMCU" e sem senha.
-
-  rtc.begin();
-  // Setar no RTC a hora que este sketch foi compilado
-   rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-  
+    
+    // Define o ESP8266 como Acess Point.
+    WiFi.mode(WIFI_AP);
+    // Cria um WiFi de nome "NodeMCU" e sem senha.
+    WiFi.softAP(ssid, password);
+    
+    rtc.begin();
+    // Setar no RTC a hora que este sketch foi compilado
+    // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    
     if (SPIFFS.begin())
     {
-//        SPIFFS.format();
-//        ftp_srv.begin(ftp_user, ftp_password);
+        SPIFFS.format();
+        // ftp_srv.begin(ftp_user, ftp_password);
+        DateTime now = rtc.now();
         File f = SPIFFS.open("/coleta.csv", "a");
-        f.println("<<< SISTEMA INICIADO >>>");
+        f.print("< SISTEMA JUAWMS - INICIO - ");
+        f.print(now.unixtime());
+        f.println(" >");
         f.close();
     }
-//    
-    Serial.println("Aguarde. Calibrando...");
+    
     sensor.calibrate();
-    Serial.println("Fim da calibração");
-
+    
     timerId = timer.setInterval(1000, modo_sensor);
     timerId2 = timer.setInterval(1000, modo_telemetria);
     timer.disable(timerId2);
@@ -77,120 +80,102 @@ void setup() {
 
 void loop() 
 {
-  button.check();
-  timer.run();
-  if (servidor_ftp)
-    ftp_srv.handleFTP();
+    button.check();
+    timer.run();
+    if (servidor_ftp)
+        ftp_srv.handleFTP();
 }
 
 void modo_telemetria()
 {
-//  if (WiFi.status() != WL_CONNECTED)
-//  {
-//    WiFi.begin(ssid, password);
-//    delay(2000);
-//  }
-  // Inicializando o servidor FTP
-  if (!servidor_ftp)
-  {
-    ftp_srv.begin(ftp_user, ftp_password);
-    servidor_ftp = true; 
-  }
-  blink_led();
+    // Inicializando o servidor FTP
+    if (!servidor_ftp)
+    {
+        ftp_srv.begin(ftp_user, ftp_password);
+        servidor_ftp = true; 
+    }
+    blink_led();
 }
 
 void modo_sensor()
 {
-  float I = sensor.getCurrentAC(60);
-  if (I > treshold && !ligado)
-  {
     // Formato: TIMESTAMP,<ON/OFF>,CORRENTE(A)
-    DateTime now = rtc.now();
-    File f = SPIFFS.open("/coleta.csv", "a");
-    f.print(now.year(), DEC);
-    f.print("-");
-    f.print(now.month(), DEC);
-    f.print("-");
-    f.print(now.day(), DEC);
-    f.print(" ");
-    f.print(now.hour(), DEC);
-    f.print(":");
-    f.print(now.minute(), DEC);
-    f.print(":");
-    f.print(now.second(), DEC);
-    f.print(",");
-    f.print("1");
-    f.print(",");
-    f.println(I);
-    f.close();
-    ligado = true;
-  }
-  else if (I < treshold && ligado)
-  {
-    DateTime now = rtc.now();
-    File f = SPIFFS.open("/coleta.csv", "a");
-    f.print(now.year(), DEC);
-    f.print("-");
-    f.print(now.month(), DEC);
-    f.print("-");
-    f.print(now.day(), DEC);
-    f.print(" ");
-    f.print(now.hour(), DEC);
-    f.print(":");
-    f.print(now.minute(), DEC);
-    f.print(":");
-    f.print(now.second(), DEC);
-    f.print(",");
-    f.print("0");
-    f.print(",");
-    f.println(I);
-    f.close();
-    ligado = false;
-  }
-  led_on();
+    float I = sensor.getCurrentAC(60);
+    if ((I >= treshold) && !ligado)
+    {
+        DateTime now = rtc.now();
+        File f = SPIFFS.open("/coleta.csv", "a");
+        f.print(now.unixtime());
+        f.print(",");
+        f.print("1");
+        f.print(",");
+        f.println(I);
+        f.close();
+        ligado = true;
+    }
+    else if ((I < treshold) && ligado)
+    {
+        DateTime now = rtc.now();
+        File f = SPIFFS.open("/coleta.csv", "a");
+        f.print(now.unixtime());
+        f.print(",");
+        f.print("0");
+        f.print(",");
+        f.println(I);
+        f.close();
+        ligado = false;
+    }
+//    else
+//    {
+//      DateTime now = rtc.now();
+//      File f = SPIFFS.open("/coleta.csv", "a");
+//      f.print(now.unixtime());
+//      f.print(",");
+//      f.println(I);
+//      f.close();
+//    }
+    led_on();
 }
 
 void blink_led()
 {
-  unsigned long current_millis = millis();
-  if(current_millis - previous_millis >= interval)
-  {
-    previous_millis = current_millis;   
-    if (led_state == LOW)
-      led_state = HIGH;  // Note that this switches the LED *off*
-    else
-      led_state = LOW;   // Note that this switches the LED *on*
-    digitalWrite(LED_POWER, led_state);
-  }
+    unsigned long current_millis = millis();
+    if(current_millis - previous_millis >= interval)
+    {
+        previous_millis = current_millis;   
+        if (led_state == LOW)
+            led_state = HIGH;
+        else
+            led_state = LOW;
+            
+        digitalWrite(LED_POWER, led_state);
+    }
 }
 
 void led_on()
 {
-  if (!led_state)
-  {
-    led_state = HIGH;
-    digitalWrite(LED_POWER, led_state);
-  }
-  
-  if (servidor_ftp)
-  {
-    servidor_ftp = false;
-//    WiFi.disconnect(true);
-  }
+    if (!led_state)
+    {
+        led_state = HIGH;
+        digitalWrite(LED_POWER, led_state);
+    }
+    
+    if (servidor_ftp)
+        servidor_ftp = false;
 }
 
 void handle_event(AceButton* /* button */, uint8_t eventType, uint8_t buttonState)
 {
-  switch (eventType)
-  {
-    case AceButton::kEventClicked:
-      timer.enable(timerId);
-      timer.disable(timerId2);
-      break;
-    case AceButton::kEventLongPressed:
-      timer.enable(timerId2);
-      timer.disable(timerId);
-      break;
-  }
+    switch (eventType)
+    {
+        case AceButton::kEventClicked:
+            timer.enable(timerId);
+            timer.disable(timerId2);
+            break;
+        case AceButton::kEventLongPressed:
+            timer.enable(timerId2);
+            timer.disable(timerId);
+            break;
+    }
 }
 
