a7a33a9d0a3fe4f5456344abed98835d0814d9ab
Daniel Carvalho <danieljosecarvalho@gmail.com>
2018-12-16 11:27:14 -0300

    Mudança no código para coletar os valores de um plug específico

--- software/main.r
+++ software/main.r
@@ -1,32 +1,57 @@
+library(anytime)
+library(forecast)
+library(data.table)
+
 setwd("DEBS/")
 
-media_por_casa <- function(arquivo_csv, nome_diretorio) {
-    # Lendo o CSV base, desconsiderando os titulo das colunas, pois nao ha
-    dados_lidos <- read.csv(arquivo_csv, header=FALSE)
-    # Coletando os valores presentes na coluna de vizinhanca sem duplicidade, retornando um vetor
-    household_ids <- unique(dados_lidos$V3)
-    # Iterando entre as vizinhancas
-    for (i in household_ids) {
-        # Gerando um subdataframe a partir do dataframe original com apenas as casas da respectiva vizinhanca
-        dados_por_household <- dados_lidos[which(dados_lidos$V3 == i), ]
-        # Coletando os ids das casas da vizinhanca sem duplicidade, retornando um vetor
-        houses_ids <- unique(dados_por_household$V4)
-        # Iterando entre as casas da vizinhanca
-        for (j in houses_ids) {
-            # Gerando um subdataframe a partir do subdataframe gerado para a vizinhanca
-            dados_house_household <- dados_por_household[which(dados_por_household$V4 == j), ]
-            # Calculando a media de consumo da casa
-            media <- mean(dados_house_household$V2)
-            # Criando uma coluna de nome 'Media', armazenando o valor da media
-            dados_house_household$Media <- media
-            # Criando um diretorio para armazenar o subdataframe da casa
-            dir.create(nome_diretorio, showWarnings=FALSE)
-            # Criando um nome para o arquivo csv a ser gerado, contendo o id da vizinhanca seguido pelo id da casa
-            name <- paste(nome_diretorio, "/", "dados_", i, "_", j, sep = "")
-            # Escrevendo no arquivo csv os dados
-            write.csv(dados_house_household, name)
-        }
-    }
+# TODO - função com erros, refazer
+# media_por_casa <- function(arquivo_csv, nome_diretorio) {
+#     # Lendo o CSV base, desconsiderando os titulo das colunas, pois nao ha
+#     dados_lidos <- read.csv(arquivo_csv, header=FALSE)
+#     # Coletando os valores presentes na coluna de vizinhanca sem duplicidade, retornando um vetor
+#     household_ids <- unique(dados_lidos$V3)
+#     # Iterando entre as vizinhancas
+#     for (i in household_ids) {
+#         # Gerando um subdataframe a partir do dataframe original com apenas as casas da respectiva vizinhanca
+#         dados_por_household <- dados_lidos[which(dados_lidos$V3 == i), ]
+#         # Coletando os ids das casas da vizinhanca sem duplicidade, retornando um vetor
+#         houses_ids <- unique(dados_por_household$V4)
+#         # Iterando entre as casas da vizinhanca
+#         for (j in houses_ids) {
+#             # Gerando um subdataframe a partir do subdataframe gerado para a vizinhanca
+#             dados_house_household <- dados_por_household[which(dados_por_household$V4 == j), ]
+#             # Calculando a media de consumo da casa
+#             media <- mean(dados_house_household$V2)
+#             # Criando uma coluna de nome 'Media', armazenando o valor da media
+#             dados_house_household$Media <- media
+#             # Criando um diretorio para armazenar o subdataframe da casa
+#             dir.create(nome_diretorio, showWarnings=FALSE)
+#             # Criando um nome para o arquivo csv a ser gerado, contendo o id da vizinhanca seguido pelo id da casa
+#             name <- paste(nome_diretorio, "/", "dados_", i, "_", j, sep = "")
+#             # Escrevendo no arquivo csv os dados
+#             write.csv(dados_house_household, name)
+#         }
+#     }
+# }
+
+hrs <- function(u) {
+ x <- u * 3600
+ return(x)
+}
+ 
+mns <- function(m) {
+ x <- m * 60
+ return(x)
 }
 
-media_por_casa("sorted_filtered_50mi.csv", "casa_por_vizinhanca")
+# PRIMEIRA PARTE
+# dados <- read.csv("sorted_tempo.csv", header=FALSE)
+# dados$tempo <- anytime(dados$V1)
+# dados$tempo
+# write.csv(dados, "sorted_tempo.csv")
+
+dados <- read.csv("sorted_tempo.csv")
+dt <- data.table(dados)
+dt[ , Date:= as.Date( tempo ) ]
+#  Get last row of each group
+dt[ , .SD[.N] ,  by = c("Date") ]
